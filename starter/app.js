a60121c8e93bf0aab0fe099397f342bc1f229b65
marwanbukhori <marwanbukhori.dev@gmail.com>
2022-08-15 16:25:20 +0800

    hide creditentials using .env file

--- starter/app.js
+++ starter/app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
-
 const connectDB = require("./db/connect");
+
+require("dotenv").config();
 
 //middleware
 app.use(express.json());
@@ -18,7 +19,7 @@ const port = 3000;
 
 const start = async() => {
     try {
-        await connectDB();
+        await connectDB(process.env.MONGO_URI);
         app.listen(port, () => console.log(`Server started on port ${port}`));
     } catch (err) {
         console.log(err.message);
--- starter/db/connect.js
+++ starter/db/connect.js
@@ -1,14 +1,11 @@
 const mongoose = require("mongoose");
 
-const connectionString =
-    "mongodb+srv://marwanbukhori:Mmmb0501@nodeexpressproject.ytxgljk.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority";
-
 const connectDB = (url) => {
-    return mongoose.connect(connectionString, {
-        usenewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
+    return mongoose.connect(url, {
+        // maxPoolSize: 50,
         useUnifiedTopology: true,
+        // wtimeoutMS: 2500,
+        useNewUrlParser: true,
     });
 };
 
