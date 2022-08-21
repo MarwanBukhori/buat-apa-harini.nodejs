16a2af6f538b2a555c0c3ce2f894895622097d34
marwanbukhori <marwanbukhori.dev@gmail.com>
2022-08-21 18:48:54 +0800

    update path

--- app.js
+++ app.js
@@ -9,11 +9,6 @@ require("dotenv").config();
 app.use(express.static("./public"));
 app.use(express.json());
 
-//routes
-app.get("/hello", (req, res) => {
-    res.send("Task Manager App");
-});
-
 app.use("/api/v1/tasks", tasks);
 
 const port = 3000;
